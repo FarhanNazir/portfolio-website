29332b18ebd97560210722c1129ea92ff44832d7
Farhan Nazir <farhannazir2660@gmail.com>
2022-06-14 23:59:28 +0800

    create header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
@@ -13,13 +12,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> e673d779bcdf9b0b886b9e5525e4896219e3005f
