37603b402b94c8573dfbfa3a4fbb72f274b97a88
Farhan Nazir <farhannazir2660@gmail.com>
2022-06-16 07:14:56 +0800

    create drop down menu

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import Logo from "../img/logo.png";
+import React, { useState, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const handleClick = (e) => {
     e.preventDefault();
     const target = e.target.getAttribute("href");
@@ -13,21 +20,97 @@ const Header = () => {
     });
   };
 
+  function showSettings(event) {
+    event.preventDefault();
+  }
+
   return (
-    <nav className=" mx-auto px-20 py-[20px] shadow-md sticky top-0  bg-indigo-600">
+    <nav className=" mx-auto px-20 py-[20px] shadow-md sticky top-0 bg-white ">
       <div className="flex items-center justify-between">
         <div className="font-bold text-lg">FARHAN NAZIR</div>
         <div className="hidden md:flex space-x-6 text-lg font-bold ">
           <a href="#home" onClick={handleClick} className="hover:text-cyan-500">
             Home
           </a>
-          <a href="#work" onClick={handleClick}  className="hover:text-cyan-500">
+          <a href="#work" onClick={handleClick} className="hover:text-cyan-500">
             Work
           </a>
-          <a href="#about" onClick={handleClick}  className="hover:text-cyan-500">
+          <a
+            href="#about"
+            onClick={handleClick}
+            className="hover:text-cyan-500"
+          >
             About
           </a>
         </div>
+        <Menu as="div" className="md:hidden relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-3 py-0.5 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+              Options
+              <ChevronDownIcon
+                className="-mr-1 ml-2 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#Home"
+                      onClick={handleClick}
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      Home
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#work"
+                      onClick={handleClick}
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      Work
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#about"
+                      onClick={handleClick}
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm"
+                      )}
+                    >
+                      About
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
     </nav>
   );
