f0c4c2d8c504825ed4a7f49c07af9ef4423cde39
Farhan Nazir <farhannazir2660@gmail.com>
2022-06-16 19:44:44 +0800

    fix mobile view

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -22,9 +22,9 @@ const Header = () => {
   };
 
   return (
-    <nav className=" mx-auto px-20  shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
+    <nav className=" mx-auto px-6 md:px-20 dark:text-cyan-300 shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
       <div className="flex items-center justify-between h-16">
-        <div className="font-bold text-lg ">FARHAN NAZIR</div>
+        <div className="font-bold text-lg  ">FARHAN NAZIR</div>
         <div className="flex items-center space-x-6">
           <div className="hidden md:flex space-x-6 text-lg font-bold ">
             <a
@@ -82,74 +82,80 @@ const Header = () => {
               />
             </svg>
           )}
-        </div>
-        <Menu as="div" className="md:hidden relative inline-block text-left">
-          <div>
-            <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm p-0.75 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-              <ChevronDownIcon
-                className=" h-[40px] w-[40px]"
-                aria-hidden="true"
-              />
-            </Menu.Button>
-          </div>
+          <Menu as="div" className="md:hidden relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm p-0.75 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                <ChevronDownIcon
+                  className=" h-[40px] w-[40px]"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
 
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#Home"
-                      onClick={handleClick}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block px-4 py-2 text-sm"
-                      )}
-                    >
-                      Home
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#work"
-                      onClick={handleClick}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block px-4 py-2 text-sm"
-                      )}
-                    >
-                      Work
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#about"
-                      onClick={handleClick}
-                      className={classNames(
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                        "block px-4 py-2 text-sm"
-                      )}
-                    >
-                      About
-                    </a>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#Home"
+                        onClick={handleClick}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Home
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#work"
+                        onClick={handleClick}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Work
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#about"
+                        onClick={handleClick}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        About
+                      </a>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
       </div>
     </nav>
   );
