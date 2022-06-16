feb3e4b2571224ff6e32c65d2b83e97ed9d4b0d5
Farhan Nazir <farhannazir2660@gmail.com>
2022-06-17 01:34:13 +0800

    add icon and fix dark mode

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -21,9 +21,22 @@ const Header = () => {
   };
 
   return (
-    <nav className=" mx-auto px-2 md:px-20 dark:text-cyan-300 shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
+    <nav className=" mx-auto px-2 md:px-20 dark:text-white shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
       <div className="flex items-center justify-between h-16">
-        <div className="font-bold text-base  ">FARHAN NAZIR</div>
+        <div className="font-bold text-base flex items-center  gap-1 m-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4   w-4 my-[10px] "
+            enableBackground="new 0 0 121.6 121.6"
+            version="1.1"
+            fill={"currentColor"}
+            viewBox="0 0 121.6 121.6"
+            xmlSpace="preserve"
+          >
+            <path d="M30 108.785c9 4.601 19.1 7 29.2 7.2h1.6c10.2 0 20.3-2.2 29.5-6.6 8.399-4 16-10 21.601-17.5 6.399-8.601 9.699-18.9 9.699-29.601v-20.3-22.1-7.7c0-4.7-4.699-7.8-9-6.1l-22.8 9.5c-1.5.6-3.2.7-4.7.1-7.4-2.9-15.6-4.4-24.3-4.4s-16.9 1.6-24.3 4.4c-1.5.6-3.2.5-4.7-.1L9 6.385c-4.3-1.8-9 1.4-9 6.1v51.1c0 9.9 3.2 19.5 8.9 27.5 5.4 7.5 12.8 13.5 21.1 17.7zm53-46.3c5.5 0 10 4.5 10 10s-4.5 10-10 10-10-4.5-10-10c0-5.599 4.5-10 10-10zm-44.4 0c5.5 0 10 4.5 10 10s-4.5 10-10 10-10-4.5-10-10c0-5.599 4.5-10 10-10z"></path>
+          </svg>
+          Farhan Nazir
+        </div>
         <div className="flex items-center md:space-x-6 space-x-3">
           <div className="hidden md:flex space-x-6 text-lg font-bold ">
             <a
@@ -100,8 +113,8 @@ const Header = () => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="origin-top-right absolute right-0 mt-[13px] w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
+              <Menu.Items className="origin-top-right absolute right-0 mt-[13px] w-56 rounded-md shadow-lg  bg-white dark:bg-gray-800  ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1  text-lg font-bold">
                   <Menu.Item>
                     {({ active }) => (
                       <a
@@ -109,8 +122,8 @@ const Header = () => {
                         onClick={handleClick}
                         className={classNames(
                           active
-                            ? "bg-gray-100 text-gray-900"
-                            : "text-gray-700",
+                            ? "bg-gray-100 text-gray-900 dark:bg-gray-500 dark:text-white"
+                            : "text-gray-700 dark:text-white",
                           "block px-4 py-2 text-sm"
                         )}
                       >
@@ -125,8 +138,8 @@ const Header = () => {
                         onClick={handleClick}
                         className={classNames(
                           active
-                            ? "bg-gray-100 text-gray-900"
-                            : "text-gray-700",
+                            ? "bg-gray-100 text-gray-900 dark:bg-gray-500 dark:text-white"
+                            : "text-gray-700 dark:text-white",
                           "block px-4 py-2 text-sm"
                         )}
                       >
@@ -141,8 +154,8 @@ const Header = () => {
                         onClick={handleClick}
                         className={classNames(
                           active
-                            ? "bg-gray-100 text-gray-900"
-                            : "text-gray-700",
+                            ? "bg-gray-100 text-gray-900 dark:bg-gray-500 dark:text-white"
+                            : "text-gray-700 dark:text-white",
                           "block px-4 py-2 text-sm"
                         )}
                       >
