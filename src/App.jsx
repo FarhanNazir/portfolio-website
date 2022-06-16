7f604a606ee080c1e9bf72a21b365d64885e0f22
Farhan Nazir <farhannazir2660@gmail.com>
2022-06-16 23:23:47 +0800

    fix the icon and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,26 @@ function App() {
   return (
     <div>
       <Header />
-      <section
-        className="min-h-[calc(100vh-64px)] bg-blue-400 font-bold flex items-center justify-center"
-        id="home"
-      >
-        <h1 className="text-5xl">Home</h1>
-      </section>
-      <section
-        className="min-h-[calc(100vh-64px)]  bg-red-400 font-bold flex items-center justify-center"
-        id="work"
-      >
-        <h1 className="text-5xl">Work</h1>
-      </section>
-      <section
-        className="min-h-[calc(100vh-64px)]  bg-yellow-300 transition-colors duration-700 dark:bg-purple-400 font-bold flex items-center justify-center"
-        id="about"
-      >
-        <h1 className="text-5xl">About</h1>
-      </section>
+      <div className=" transition-colors duration-700 dark:bg-cyan-700 ">
+        <section
+          className="min-h-[calc(100vh-64px)]  font-bold flex items-center justify-center"
+          id="home"
+        >
+          <h1 className="text-5xl">Home</h1>
+        </section>
+        <section
+          className="min-h-[calc(100vh-64px)]  font-bold flex items-center justify-center"
+          id="work"
+        >
+          <h1 className="text-5xl">Work</h1>
+        </section>
+        <section
+          className="min-h-[calc(100vh-64px)]   font-bold flex items-center justify-center"
+          id="about"
+        >
+          <h1 className="text-5xl">About</h1>
+        </section>
+      </div>
     </div>
   );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState, Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
 import useDarkMode from "../useDarkMode";
 
 function classNames(...classes) {
@@ -22,7 +21,7 @@ const Header = () => {
   };
 
   return (
-    <nav className=" mx-auto px-6 md:px-20 dark:text-cyan-300 shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
+    <nav className=" mx-auto px-2 md:px-20 dark:text-cyan-300 shadow-md sticky top-0 bg-white transition-colors duration-700 dark:bg-gray-900 ">
       <div className="flex items-center justify-between h-16">
         <div className="font-bold text-base  ">FARHAN NAZIR</div>
         <div className="flex items-center md:space-x-6 space-x-3">
@@ -49,45 +48,46 @@ const Header = () => {
               About
             </a>
           </div>
-          <div className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm p-0.75 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+          <div className="inline-flex justify-center w-full rounded-md border shadow-sm p-0.75  dark:bg-orange-300 bg-purple-400 text-sm font-medium text-gray-700">
             {colorTheme === "light" ? (
-              <svg
-                onClick={() => setTheme("light")}
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-[20px] w-[20px] m-2  "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                />
-              </svg>
+              <button onClick={() => setTheme("light")}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="h-[20px] w-[20px] m-2 "
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                </svg>
+              </button>
             ) : (
-              <svg
-                onClick={() => setTheme("dark")}
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-[20px] w-[20px] m-2  text-gray-900"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
+              <button onClick={() => setTheme("dark")}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-[20px] w-[20px] m-2 "
+                  viewBox="0 0 20 20"
+                  fill="white"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                </svg>
+              </button>
             )}
           </div>
           <Menu as="div" className="md:hidden relative inline-block text-left">
             <div>
-              <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm p-0.75 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                <ChevronDownIcon className=" h-[20px] w-[20px] m-2 " />
+              <Menu.Button className="inline-flex justify-center w-full rounded-md border shadow-sm p-0.75 transition-colors duration-700  dark:bg-gray-900  bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-[20px] w-[20px] m-2 "
+                  viewBox="0 0 20 20"
+                  fill={colorTheme === "light" ? "white" : "black"}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </Menu.Button>
             </div>
 
